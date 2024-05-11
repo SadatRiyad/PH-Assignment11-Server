9d191d578ccb2edba3919f56e5704cd708b872b1
SadatRiyad <sadatriyad@students.diu.ac>
2024-05-11 20:24:26 +0600

    server connect with mongodb successfully

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.efrqq6z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 // middleware
-app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ph-assignment11-sadatriyad.surge.sh",
+      "https://ph-assignment11-sadatriyad.netlify.app",
+      "*", // allow all origins
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
-// routes
+// Routes
 app.get("/", (req, res) => {
-  res.send("BB-QueryHub Server is Running...");
+  res.send("BB-QueryHub server is running");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+async function run() {
+  const QueryHubCollection = client
+    .db("BB-QueryHubDB")
+    .collection("Queries");
+  try {
+    // Get all the data from the collection
+    app.get("/Queries", async (req, res) => {
+      const data = QueryHubCollection.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+// Listen for incoming requests
+app.listen(port, () => console.log(`Server is running on port ${port}`));
