de86bf7e3010103effa699f887146a2bc918659c
SadatRiyad <sadatriyad@students.diu.ac>
2024-05-15 20:59:03 +0600

    jws create ang logout added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,6 +15,15 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+
+// Let's create a cookie options for both production and local server
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+//localhost:5000 and localhost:5173 are treated as same site.  so sameSite value must be strict in development server.  in production sameSite will be none
+// in development server secure will false .  in production secure will be true
 
 // middleware
 app.use(express.json());
@@ -196,6 +207,24 @@ async function run() {
       } else {
         res.status(500).json({ error: "Failed to add recommendation" });
       }
+    });
+
+    //creating Token
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    //clearing Token
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
     });
   } finally {
     // Ensures that the client will close when you finish/error
